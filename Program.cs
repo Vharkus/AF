2be4ac594ed9e19b1bb69f02e2e268ea168bb14c
aluno <aluno@A2723>
2023-06-05 22:38:14 -0300

    Update

--- Program.cs
+++ Program.cs
@@ -3,18 +3,18 @@ using System;
 
 namespace Repeticao
 {
-    class Program
+    class DoWhile
     {
         static void Main(string[] args)
         {
-            int num, i = 1;
-            Console.WriteLine("Digite um número inteiro positivo: ");
-            num = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            while (i <= num)
+            int num = 0;
+
+            do
             {
-                Console.WriteLine(i);
-                i++;
-            }
+                Console.WriteLine("Digite um número entre 1 e 10: ");
+                num = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            } while (num < 1 || num > 10);
+            Console.WriteLine("Você digitou " + num + " .");
             return;
         }
     }
