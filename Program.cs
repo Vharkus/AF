6e463843be8a7942c9fd435bb2ab1a4a65ec8f86
aluno <aluno@A2723>
2023-06-05 22:26:54 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using System;
+
+namespace Repeticao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, i = 1;
+            Console.WriteLine("Digite um número inteiro positivo: ");
+            num = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            while (i <= num)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            return;
+        }
+    }
+}
