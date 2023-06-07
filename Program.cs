3c0dc8d5e2c0a8848c36445170dfa48e8142d8e6
Shinji Nakano <127798992+Vharkus@users.noreply.github.com>
2023-06-07 14:06:07 -0300

    Update.cs

--- Program.cs
+++ Program.cs
@@ -1,21 +1,23 @@
-﻿using System.Globalization;
+//Biblioteca padrão
 using System;
-
-namespace Repeticao
-{
-    class DoWhile
-    {
+//Classe do código.
+class Program
+    {   //Corpo principal do código. Aqui há um string com array que permite adicionar novos códigos e argumentos.
         static void Main(string[] args)
-        {
-            int num = 0;
-
-            do
-            {
-                Console.WriteLine("Digite um número entre 1 e 10: ");
-                num = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            } while (num < 1 || num > 10);
-            Console.WriteLine("Você digitou " + num + " .");
-            return;
+        {   //Declaração da variável.
+            int num;
+            //Interface interativa.
+            Console.Write("Digite um número inteiro positivo: ");
+            //Aqui a informação anterior será lida e armazenada na variável em questão.
+            num = int.Parse(Console.ReadLine());
+            //Interface que mostra a quantidade de números pares que serão exibidos.
+            Console.Write("Os primeiros " + num + " são pares ");
+            //Estrutura de repetição. O i vale 2 e enquanto o i for menor ou igual ao número digitado o código ficará repetindo. O contador final força o resultado somar 2 o tempo todo.
+            for (int i = 2; i <= num * 2; i += 2)
+            {   //Interface que soma o valor de i e gerá nosso número.
+                 Console.Write(i + " ");
+            }
+            //Quebra de linha após a exibição dos números            
+            Console.WriteLine();
         }
     }
-}
