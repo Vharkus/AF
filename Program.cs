e918cd4ff7cb1d10067176b1a4476dc23792bada
Shinji Nakano <127798992+Vharkus@users.noreply.github.com>
2023-06-07 14:24:42 -0300

    Update.cs

--- Program.cs
+++ Program.cs
@@ -1,21 +1,50 @@
-﻿using System.Globalization;
+//Biblioteca padrão.
 using System;
-
-namespace Repeticao
-{
-    class DoWhile
-    {
-        static void Main(string[] args)
-        {
-            int num = 0;
-
-            do
-            {
-                Console.WriteLine("Digite um número entre 1 e 10: ");
-                num = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            } while (num < 1 || num > 10);
-            Console.WriteLine("Você digitou " + num + " .");
-            return;
-        }
+//Classe do código principal. Ele guarda as informações dos métodos usados para que o código funcione.
+class Program
+{   //Declaração da variável que permite realisar a adição dos números.
+    float Soma(float a, float b)
+    {   //Método da classe. Aqui já definimos o tipo de resultado, como vamos obtê-lo e o seu retorno.
+        float Resultado;
+        Resultado = a + b;
+        return Resultado;
+    }
+    //Declaração da variável que permite realizar a subtração dos números.
+    float Subt(float a, float b)
+    {   //Método da classe. Aqui já definimos o tipo de resultado, como vamos obtê-lo e o seu retorno.
+        float Resultado;
+        Resultado = a - b;
+        return Resultado;
+    }
+    //Declaração da variável que permite realizar a multiplicação dos números.
+    float Multi(float a, float b)
+    {   //Método da classe. Aqui já definimos o tipo de resultado, como vamos obtê-lo e o seu retorno.
+        float Resultado;
+        Resultado = a * b;
+        return Resultado;
+    }
+    //Declaração da variável que permite realizar a divisão dos números
+    float Divi(float a, float b)
+    {   //Método da classe. Aqui já definimos o tipo de resultado, como vamos obtê-lo e o seu retorno.
+        float Resultado;
+        Resultado = a / b;
+        return Resultado;
+    }
+    //Corpo principal do código.
+    static void Main()
+    {   //Declaração das variáve
+        float a, b;
+        //Interface interativa para escrever os números.
+        Console.WriteLine("Informe os números:");
+        //Forma de armazenar as respostas em cada variável respectivamente.
+        a = float.Parse(Console.ReadLine());
+        b = float.Parse(Console.ReadLine());
+        //Aqui instanciamos a classe que criamos e usamos seus métodos feitos.
+        Program programa = new Program();
+        //Usando os métodos feitos dentro da classe nós teremos a resposta respectiva de cada operador.
+        Console.WriteLine("O valor da soma é: " + programa.Soma(a, b));
+        Console.WriteLine("O valor da subtração é: " + programa.Subt(a, b));
+        Console.WriteLine("O valor da multiplicação é: " + programa.Multi(a, b));
+        Console.WriteLine("O valor da divisão é: " + programa.Divi(a, b));
     }
 }
