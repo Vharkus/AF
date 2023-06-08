a4171bcc5205d989feca81dfbd64aa14f05accad
Shinji Nakano <127798992+Vharkus@users.noreply.github.com>
2023-06-08 11:57:11 -0300

    Do While.cs

--- Do While.cs
+++ Do While.cs
@@ -1,20 +1,25 @@
-﻿using System.Globalization;
+//Bibliotecas padrões.
+using System.Globalization;
 using System;
 
 namespace Repeticao
-{
+{   //Classe principal do programa. 
     class DoWhile
-    {
+    {   //Corpo principal do código.
         static void Main(string[] args)
-        {
+        {   //Declaração da variável.
             int num = 0;
-
+            //Parte do código que será repetido.
             do
-            {
+            {   //Interface interativa que pede um número.
                 Console.WriteLine("Digite um número entre 1 e 10: ");
+                //Lê e armazena o número digitado na variável.
                 num = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+             // Verifica se o número está fora do intervalo permitido.
             } while (num < 1 || num > 10);
+             // Exibe o número digitado pelo usuário.
             Console.WriteLine("Você digitou " + num + " .");
+            //Encerra o programa
             return;
         }
     }
